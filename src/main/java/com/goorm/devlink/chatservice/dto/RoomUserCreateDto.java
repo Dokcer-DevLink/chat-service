cba518fcff437fbe5cd9577b5dca6ae9fd87fc04
mgKang3646 <lordofkangs@naver.com>
2023-12-15 01:40:26 +0900

    feat: Chat-Service dto 추가

---
+++ src/main/java/com/goorm/devlink/chatservice/dto/RoomUserCreateDto.java
@@ -0,0 +1,26 @@
+package com.goorm.devlink.chatservice.dto;
+
+
+import com.goorm.devlink.chatservice.vo.RoomUserState;
+import lombok.*;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor( access = AccessLevel.PROTECTED )
+public class RoomUserCreateDto {
+
+    private String userUuid ;
+    private int readMessageCount;
+    private RoomUserState userState;
+
+    public static RoomUserCreateDto getInstance(String userUuid){
+        return RoomUserCreateDto.builder()
+                .userUuid(userUuid)
+                .userState(RoomUserState.CREATE)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/goorm/devlink/chatservice/dto/RoomUserFindDto.java
@@ -0,0 +1,20 @@
+package com.goorm.devlink.chatservice.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class RoomUserFindDto {
+
+    private String userUuid;
+    private String targetUuid;
+
+    public static RoomUserFindDto getInstance(String userUuid, String targetUuid){
+        return RoomUserFindDto.builder()
+                .userUuid(userUuid)
+                .targetUuid(targetUuid)
+                .build();
+    }
+}

070ef701308d91308d8a6f1955bebc18bfcfc8f3
mgKang3646 <lordofkangs@naver.com>
2023-12-15 00:40:22 +0900

    feat: chat-service util 추가

---
+++ src/main/java/com/goorm/devlink/chatservice/util/ModelMapperUtil.java
@@ -0,0 +1,52 @@
+package com.goorm.devlink.chatservice.util;
+
+import com.goorm.devlink.chatservice.vo.ChatRoomResponse;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class ModelMapperUtil {
+
+    private final ModelMapper mapper;
+    public RoomUser convertToRoomUser(String userUuid){
+        return mapper.map(RoomUserCreateDto.getInstance(userUuid),RoomUser.class);
+    }
+
+    public List<ChatRoomResponse> convertToChatRoomResponseList(List<ChatRoom> chatRoomList, String sender){
+        return convertChatRoomDtoListToChatRoomResponseList(convertChatRoomListToChatRoomDtoList(chatRoomList),sender);
+    }
+
+    public ChatMessage convertToChatMessage(ChatDto chatDto, ChatRoom chatRoom){
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        ChatMessage chatMessage = mapper.map(chatDto, ChatMessage.class);
+        chatMessage.updateChatRoom(chatRoom);
+        return chatMessage;
+    }
+
+    private List<ChatRoomDto> convertChatRoomListToChatRoomDtoList(List<ChatRoom> chatRoomList){
+        List<ChatRoomDto> chatRoomDtoList = new ArrayList<>();
+        chatRoomList.forEach( chatRoom -> {
+            ChatRoomDto chatRoomDto = mapper.map(chatRoom, ChatRoomDto.class);
+            chatRoomDtoList.add(chatRoomDto);
+        });
+
+        return chatRoomDtoList;
+    }
+
+    private List<ChatRoomResponse> convertChatRoomDtoListToChatRoomResponseList(List<ChatRoomDto> chatRoomDtoList, String sender){
+
+        List<ChatRoomResponse> chatRoomResponseList = new ArrayList<>();
+        chatRoomDtoList.forEach(chatRoomDto -> {
+            chatRoomResponseList.add(ChatRoomResponse.convert(chatRoomDto,sender));
+        });
+
+        return chatRoomResponseList;
+    }
+
+}

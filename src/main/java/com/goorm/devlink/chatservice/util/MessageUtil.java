f8d17bbfb8fb4e652c364247d569e90ab4b78420
mgKang3646 <lordofkangs@naver.com>
2023-12-15 01:43:57 +0900

    feat: util 추가완료

---
+++ src/main/java/com/goorm/devlink/chatservice/util/MessageUtil.java
@@ -0,0 +1,31 @@
+package com.goorm.devlink.chatservice.util;
+
+
+import com.goorm.devlink.chatservice.dto.ChatDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
+@RequiredArgsConstructor
+public class MessageUtil {
+
+    private final MessageSource messageSource;
+
+    public String getRoomNoSuchMessage(String roomUuid){
+        return messageSource.getMessage("request.nosuch.room",new String[]{roomUuid}, Locale.KOREA);
+    }
+
+    public String getUserNoSuchMessage(ChatDto chatDto){
+        return messageSource.getMessage("request.nosuch.user", new String[]{chatDto.getRoomUuid(),
+                chatDto.getSenderUuid()},Locale.KOREA);
+    }
+
+    public String getNoEqualUserUuidMessage(String userUuid,String targetUuid){
+        return messageSource.getMessage("request.noequal.user", new String[]{userUuid,targetUuid},Locale.KOREA);
+    }
+
+    public String getNoIsoTypeMessage(){
+        return messageSource.getMessage("request.noisotype.datae",new String[] {},Locale.KOREA);
+    }
+}

14f79d914821aa6981a0a20c7df4e6a665e09470
mgKang3646 <lordofkangs@naver.com>
2023-12-15 00:45:58 +0900

    feat: chat controller 생성

---
+++ src/main/java/com/goorm/devlink/chatservice/controller/ChatRoomController.java
@@ -0,0 +1,65 @@
+package com.goorm.devlink.chatservice.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+//@CrossOrigin
+public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+    private final KafkaTemplate<String, ChatDto> kafkaTemplate;
+    private final KafkaConfigVo kafkaConfigVo;
+    private final MessageUtil messageUtil;
+
+    @PostMapping("/send")
+    public void publishMessageToTopic(@RequestBody @Valid ChatDto chatDto)
+            throws ExecutionException, InterruptedException {
+        if(chatDto.getType()==ChatDto.MessageType.TALK) chatRoomService.processSendMessage(chatDto);
+        kafkaTemplate.send(kafkaConfigVo.getTopicName(),chatDto).get();
+    }
+
+    // 채팅리스트 화면 조회
+    @GetMapping("/api/chat/rooms")
+    public ResponseEntity<List<ChatRoomResponse>> findAllChatRoomsByUserId(@RequestHeader("userUuid") @NotBlank String userUuid){
+        return new ResponseEntity<>(chatRoomService.findAllChatRoomByUserId(userUuid), HttpStatus.OK);
+    }
+
+    // 채팅방 생성
+    @GetMapping("/api/chat/createroom")
+    public ResponseEntity<ChatRoomCreateResponse> createRoom(@RequestHeader("userUuid") @NotBlank String userUuid,
+                                                             @RequestParam @NotBlank String targetUuid) {
+        if(userUuid.equals(targetUuid)) {
+            throw new IllegalArgumentException(messageUtil.getNoEqualUserUuidMessage(userUuid,targetUuid));
+        }
+        String roomUuid = chatRoomService.findOrCreateChatRoom(userUuid, targetUuid);
+        return new ResponseEntity<>(ChatRoomCreateResponse.getInstance(roomUuid), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/chat")
+    public ResponseEntity<Slice<ChatDto>> findAllChatList(@RequestParam("roomUuid") @NotBlank String roomUuid,
+                                                          @RequestParam("beforeTime") @NotBlank String beforeTime ){
+        return new ResponseEntity<>(chatRoomService.findAllChatList(roomUuid,
+                LocalDateTime.parse(beforeTime, DateTimeFormatter.ISO_DATE_TIME)),HttpStatus.OK);
+    }
+
+
+
+}

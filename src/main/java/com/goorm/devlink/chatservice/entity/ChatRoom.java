26b07b15817730f5c6ddf9c6de9897148d9844c2
mgKang3646 <lordofkangs@naver.com>
2023-12-15 00:44:20 +0900

    feat: chat-service entity 추가

---
+++ src/main/java/com/goorm/devlink/chatservice/entity/ChatRoom.java
@@ -0,0 +1,59 @@
+package com.goorm.devlink.chatservice.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor( access = AccessLevel.PROTECTED )
+@AllArgsConstructor
+@Getter
+@Setter
+@SequenceGenerator(
+        name= "ROOM_SEQ_GENERATOR",
+        sequenceName = "CHAT_ROOM_SEQ",
+        initialValue = 1, allocationSize = 1
+)
+public class ChatRoom extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "ROOM_SEQ_GENERATOR")
+    @Column(name = "room_id")
+    private Long roomId;
+
+    @Column(name = "room_uuid", unique = true)
+    private String roomUuid;
+
+    @Column(name = "recent_message")
+    private String recentMessage;
+
+    @Column(name = "recent_date")
+    private LocalDateTime recentDate;
+
+    @Column(name = "message_count", nullable = false)
+    private int messageCount;
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<RoomUser> roomUsers = new ArrayList<>();
+
+    @OneToMany(mappedBy="chatRoom")
+    private List<ChatMessage> chatMessages = new ArrayList<>();
+
+    public static ChatRoom getInstance(String roomUuid){
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.setRoomUuid(roomUuid);
+        return chatRoom;
+    }
+
+    public static ChatRoom getInstance( ){
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.setRoomUuid(UUID.randomUUID().toString());
+        return chatRoom;
+    }
+}
---
+++ src/main/java/com/goorm/devlink/chatservice/entity/RoomUser.java
@@ -0,0 +1,43 @@
+package com.goorm.devlink.chatservice.entity;
+
+
+import com.goorm.devlink.chatservice.vo.RoomUserState;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@SequenceGenerator(
+        name= "USER_SEQ_GENERATOR",
+        sequenceName = "CHAT_USER_SEQ",
+        initialValue = 1, allocationSize = 1
+)
+public class RoomUser extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "USER_SEQ_GENERATOR")
+    @Column(name = "room_user_id")
+    private Long roomUserId;
+    @Column( name = "user_uuid", nullable = false )
+    private String userUuid;
+    @Column( name="read_message_count", nullable = false )
+    private int readMessageCount;
+    @Enumerated(EnumType.STRING)
+    @Column ( name = "user_state" )
+    private RoomUserState userState;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="room_id")
+    private ChatRoom chatRoom;
+
+    public void updateChatRoom(ChatRoom chatRoom) {
+        this.chatRoom = chatRoom;
+    }
+
+
+}

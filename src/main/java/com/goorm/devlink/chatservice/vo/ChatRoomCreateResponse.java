5abe42cb1e7deb573c3a80b057964a19ad5c6918
mgKang3646 <lordofkangs@naver.com>
2023-12-15 01:39:55 +0900

    feat: Chat-Service vo 추가

---
+++ src/main/java/com/goorm/devlink/chatservice/vo/ChatRoomCreateResponse.java
@@ -0,0 +1,17 @@
+package com.goorm.devlink.chatservice.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ChatRoomCreateResponse {
+
+    private String roomUuid;
+
+    public static ChatRoomCreateResponse getInstance(String roomUuid){
+        return ChatRoomCreateResponse.builder()
+                .roomUuid(roomUuid)
+                .build();
+    }
+}
---
+++ src/main/java/com/goorm/devlink/chatservice/vo/ChatRoomResponse.java
@@ -0,0 +1,47 @@
+package com.goorm.devlink.chatservice.vo;
+
+
+
+import com.goorm.devlink.chatservice.dto.ChatRoomDto;
+import com.goorm.devlink.chatservice.dto.RoomUserCreateDto;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor( access = AccessLevel.PROTECTED)
+public class ChatRoomResponse {
+
+    private String roomUuid;
+    private String targetUuid;
+    private String recentMessage;
+    private LocalDateTime recentDate;
+    private int messageNotRead;
+
+    public static ChatRoomResponse getInstance(String roomUuid){
+        return ChatRoomResponse.builder()
+                .roomUuid(roomUuid)
+                .build();
+    }
+
+    public static ChatRoomResponse convert(ChatRoomDto chatRoomDto, String sender){
+        ChatRoomResponse chatRoomResponse = new ChatRoomResponse();
+        for (RoomUserCreateDto roomUser : chatRoomDto.getRoomUsers()) {
+            if(!roomUser.getUserUuid().equals(sender)) {
+                chatRoomResponse.setRoomUuid(chatRoomDto.getRoomUuid()); // RoomId
+                chatRoomResponse.setTargetUuid(roomUser.getUserUuid()); // 대화상대 userId
+                chatRoomResponse.setRecentMessage(chatRoomDto.getRecentMessage()); // 가장 최근 대화 메시지
+                chatRoomResponse.setRecentDate(chatRoomDto.getRecentDate()); // 가장 최근 대화 시간
+            }else{ // 자기 자신인 경우
+                chatRoomResponse.setMessageNotRead((chatRoomDto.getMessageCount() - roomUser.getReadMessageCount())); // 안 읽은 메시지 개수
+            }
+        }
+
+        return chatRoomResponse;
+    }
+
+
+}

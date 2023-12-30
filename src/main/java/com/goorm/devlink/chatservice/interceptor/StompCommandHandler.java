534207de8af3395a8f136cc8215634fa335c23d1
mgKang3646 <lordofkangs@naver.com>
2023-12-30 22:05:50 +0900

    chore: interceptor 로그 추가

--- src/main/java/com/goorm/devlink/chatservice/interceptor/StompCommandHandler.java
+++ src/main/java/com/goorm/devlink/chatservice/interceptor/StompCommandHandler.java
@@ -8,6 +8,8 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 
 @Slf4j
 @Component
@@ -21,10 +23,19 @@ public class StompCommandHandler implements ChannelInterceptor {
             headerAccessor.getSessionAttributes().put("isUnsubscribe",true);
         }
 
+        log.info("[CHAT-SERVICE] {} 요청 시작 ! ", headerAccessor.getCommand());
+
         return message;
     }
 
+    @Override
+    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
 
+        log.info("[CHAT-SERVICE] {} 요청 종료  !", headerAccessor.getCommand());
+
+
+    }
 
 
 }
